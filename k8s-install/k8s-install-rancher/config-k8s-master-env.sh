93aa3ebfed42f159a34d6ea1472a21ff9d60df5d
YitianJiang <2138127826@qq.com>
2020-12-20 01:22:12 +0800

    touch ~/.kube/config

--- k8s-install/k8s-install-rancher/config-k8s-master-env.sh
+++ k8s-install/k8s-install-rancher/config-k8s-master-env.sh
@@ -29,7 +29,10 @@ systemctl enable chronyd
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
-
+if [ ! -d "/root/.kube" ]; then
+  mkdir ~/.kube
+fi
+touch ~/.kube/config
 
 
  
