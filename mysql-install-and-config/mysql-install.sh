258272fd204bfb6a2e2e04c691978bb9a10720e0
YitianJiang <36124775+YitianJiang@users.noreply.github.com>
2021-04-18 00:30:20 +0800

    fix "Job for mysqld.service failed because the control process exited with error"

--- mysql-install-and-config/mysql-install.sh
+++ mysql-install-and-config/mysql-install.sh
@@ -1,9 +1,25 @@
+#1.install wget
 if [[ `wget --version | wc -l` -lt 10 ]]; then
     yum install -y wget
 fi
 wget -i -c http://dev.mysql.com/get/mysql57-community-release-el7-10.noarch.rpm
+
+#2.install mysql
 yum -y install mysql57-community-release-el7-10.noarch.rpm
 yum -y install mysql-community-server
+
+#3.start mysql for the first time, find root password from mysqld.log, and modify the password to simple password
+systemctl start  mysqld.service
+passwordStr=`grep "A temporary password is generated for root@localhost" /var/log/mysqld.log`
+password=${passwordStr#*root@localhost: }
+mysql --connect-expired-password -uroot -p$password <<EOF
+alter user 'root'@'localhost' identified by '123456';
+grant all privileges on *.* to 'root'@'%' identified by '123456' with grant option;
+flush privileges; 
+EOF
+
+#4.modify my.cnf after the first start of mysql other than before to avoid the following:
+#"Job for mysqld.service failed because the control process exited with error"
 cat << EOF >> /etc/my.cnf 
 #添加validate_password_policy配置
 validate_password_policy=0
@@ -14,15 +30,10 @@ validate_password = off
 character_set_server=utf8
 init_connect='SET NAMES utf8'
 EOF
-systemctl start  mysqld.service
+
+#5.restart mysql and open firewall port
+systemctl restart  mysqld.service
 sleep 5
-passwordStr=`grep "A temporary password is generated for root@localhost" /var/log/mysqld.log`
-password=${passwordStr#*root@localhost: }
-mysql --connect-expired-password -uroot -p$password <<EOF
-alter user 'root'@'localhost' identified by '123456';
-grant all privileges on *.* to 'root'@'%' identified by '123456' with grant option;
-flush privileges; 
-EOF
 firewall-cmd --zone=public --add-port=3306/tcp --permanent
 firewall-cmd --reload
 
