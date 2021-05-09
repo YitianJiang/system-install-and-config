243841843cbde5fa8bb3b87ad6b46dd8afebb8e6
YitianJiang <2138127826@qq.com>
2021-05-10 02:09:03 +0800

    add zookeeper

---
+++ zookeeper/zookeeper-install-config-single-point.sh
@@ -0,0 +1,10 @@
+zkVersion=3.7.0
+if [[ $1 != "" ]]; then
+    zkVersion=$1
+cd /root
+wget https://downloads.apache.org/zookeeper/zookeeper-${zkVersion}/apache-zookeeper-${zkVersion}.tar.gz 
+tar zxvf apache-zookeeper-${zkVersion}.tar.gz
+cp /root/system-install-and-config/zookeeper/zoo.cfg /root/apache-zookeeper-${zkVersion}/conf
+mkdir /root/apache-zookeeper-${zkVersion}/data
+echo dataDir=/root/apache-zookeeper-${zkVersion}/data >>  /root/apache-zookeeper-${zkVersion}/conf/zoo.cfg
+. /root/apache-zookeeper-${zkVersion}/bin/zkServer.sh start
