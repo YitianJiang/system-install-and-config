947c4650bf52be9cfb855ae1e050c778947634b4
YitianJiang <2138127826@qq.com>
2021-05-10 04:02:04 +0800

    update openjdk-install-config.sh

--- java/openjdk-install-config.sh
+++ java/openjdk-install-config.sh
@@ -1,7 +1,11 @@
-yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel  
-echo << EOF >>/etc/profile
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64
+yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
+temp=$(dirname $(readlink $(readlink $(which java))))
+
+cat << EOF >>/etc/profile
+export JAVA_HOME=${temp%/jre/bin*}
 export CLASSPATH=.:$JAVA_HOME/jre/lib/rt.jar:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 export PATH=$PATH:$JAVA_HOME/bin
+123
 EOF
 source /etc/profile
+echo $JAVA_HOME
