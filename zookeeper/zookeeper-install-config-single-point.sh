db7339ff9b9b1e50a02230eaaec46f0660b65c74
YitianJiang <2138127826@qq.com>
2021-05-10 04:24:01 +0800

    add dataLogDir

--- zookeeper/zookeeper-install-config-single-point.sh
+++ zookeeper/zookeeper-install-config-single-point.sh
@@ -1,4 +1,4 @@
-zkVersion=3.7.0
+zkVersion=3.5.9
 if [[ $1 != "" ]]; then
     zkVersion=$1
 fi
@@ -7,5 +7,9 @@ wget https://downloads.apache.org/zookeeper/zookeeper-${zkVersion}/apache-zookee
 tar zxvf apache-zookeeper-${zkVersion}.tar.gz
 cp /root/system-install-and-config/zookeeper/zoo.cfg /root/apache-zookeeper-${zkVersion}/conf
 mkdir /root/apache-zookeeper-${zkVersion}/data
-echo dataDir=/root/apache-zookeeper-${zkVersion}/data >>  /root/apache-zookeeper-${zkVersion}/conf/zoo.cfg
+mkdir /root/apache-zookeeper-${zkVersion}/logs
+cat << EOF >> /root/apache-zookeeper-${zkVersion}/conf/zoo.cfg
+dataDir=/root/apache-zookeeper-${zkVersion}/data
+dataLogDir=/root/apache-zookeeper-${zkVersion}/logs
+EOF 
 . /root/apache-zookeeper-${zkVersion}/bin/zkServer.sh start
