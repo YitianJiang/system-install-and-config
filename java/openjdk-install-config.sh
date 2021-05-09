32de5be5a00c55629e58caea9b7e002705c0d71c
YitianJiang <2138127826@qq.com>
2021-05-10 02:48:11 +0800

    fix: no package available

--- java/openjdk-install-config.sh
+++ java/openjdk-install-config.sh
@@ -1,5 +1,4 @@
-yum install -y java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64
-yum install -y java-1.8.0-openjdk-devel-1.8.0.212.b04-0.el7_6.x86_64
+yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel  
 echo << EOF >>/etc/profile
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64
 export CLASSPATH=.:$JAVA_HOME/jre/lib/rt.jar:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
