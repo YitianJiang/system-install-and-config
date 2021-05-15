24f2f48c6d54bb7d3f31da9f86ad01f599089a02
YitianJiang <2138127826@qq.com>
2021-05-15 14:14:34 +0800

    change the position of comments to fix : '--privileged: command not found' and '--network: command not found'

--- jenkins/single-point/docker.sh
+++ jenkins/single-point/docker.sh
@@ -1,18 +1,20 @@
 #referencce: https://www.jenkins.io/doc/book/installing/docker/
 bash /root/system-install-and-config/docker-install.sh
 docker network create jenkins
+#change --rm to --restart=always
 docker run --name jenkins-docker \
-  --restart=always --detach \  #change --rm to --restart=always
+  --restart=always --detach \  
   --privileged --network jenkins --network-alias docker \
   --env DOCKER_TLS_CERTDIR=/certs \
   --volume jenkins-docker-certs:/certs/client \
   --volume jenkins-data:/var/jenkins_home \
   --publish 2376:2376 docker:dind --storage-driver overlay2
 
-docker build -t myjenkins-blueocean:1.1 .
+docker build -t myjenkins-blueocean:1.1 $(pwd)
 
+#change --rm to --restart=always
 docker run --name jenkins-blueocean \
-  --restart=always --detach \ #change --rm to --restart=always
+  --restart=always --detach \ 
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
   --publish 8080:8080 --publish 50000:50000 \
