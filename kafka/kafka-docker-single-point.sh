40b67595ace8c0346b24411d8fbea016080b6c3c
YitianJiang <2138127826@qq.com>
2021-06-24 18:27:35 +0800

    update kafka-docker-single-point.sh

--- kafka/kafka-docker-single-point.sh
+++ kafka/kafka-docker-single-point.sh
@@ -1,15 +1,17 @@
 ############################################通用##################################################################
 set -eux
 if [[ $SYSTEM_INSTALL_AND_CONFIG == "" ]]; then
-	export SYSTEM_INSTALL_AND_CONFIG=$(find / -name system-install-and-config)
+  export SYSTEM_INSTALL_AND_CONFIG=$(find / -name system-install-and-config)
 fi
 ############################################通用##################################################################
 
+#先安装zookeeper
 bash $SYSTEM_INSTALL_AND_CONFIG/zookeeper/zookeeper-docker-single-point.sh
 
 source $SYSTEM_INSTALL_AND_CONFIG/util.sh
 ip=$(getIp)
 
+#安装kafka
 docker run  -d --name kafka \
 -p 9092:9092 \
 --link zookeeper:zookeeper \
@@ -18,3 +20,12 @@ docker run  -d --name kafka \
 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://${ip}:9092 \
 -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 \
 -t wurstmeister/kafka \
+
+#安装kafkamanager
+docker run -d --name kafka-manager \
+--link zookeeper:zookeeper \
+--link kafka:kafka \
+-p 9001:9000 \
+--restart=always \
+--env ZK_HOSTS=zookeeper:2181 \
+sheepkiller/kafka-manager
