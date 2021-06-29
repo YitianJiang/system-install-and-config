2ef9e71f59047c8ab1e8e6fa541df73a54eb5147
YitianJiang <2138127826@qq.com>
2021-06-30 00:43:36 +0800

    update gitlab-docker-single-point.sh

--- gitlab/gitlab-docker-single-point.sh
+++ gitlab/gitlab-docker-single-point.sh
@@ -9,13 +9,13 @@ docker run --detach \
   gitlab/gitlab-ce:latest
 
 docker exec -i gitlab /bin/bash <<- EOF
-	cd /opt/gitlab/bin
-		gitlab-rails console <<- END
-		u=User.where(id:1).first
-		u.password='12345678'
-		u.password_confirmation='12345678'
-		u.save!
-	END   
+cd /opt/gitlab/bin
+gitlab-rails console <<- END
+u=User.where(id:1).first
+u.password='12345678'
+u.password_confirmation='12345678'
+u.save!
+END   
 EOF
 
 
