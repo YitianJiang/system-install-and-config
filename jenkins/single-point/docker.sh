86d8427ecd6269ac96a1301d3a51f4a7a8bc5f60
YitianJiang <2138127826@qq.com>
2021-06-30 01:26:08 +0800

    update jenind/single-point/docker.sh

--- jenkins/single-point/docker.sh
+++ jenkins/single-point/docker.sh
@@ -36,11 +36,10 @@
 
 docker run \
   -u root \
-  --rm \
   -d \
   -p 8086:8080 \
   -p 50000:50000 \
   -v jenkins-data:/var/jenkins_home \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  --restart=always
+  --restart=always \
   jenkinsci/blueocean
